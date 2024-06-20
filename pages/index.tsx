6767846a407ec988785e12f353609122b673fa09
AayushCodes <aayushcodes@gmail.com>
2024-06-20 22:03:29 +0530

    added subtitle for header

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,22 @@ const Home: NextPage = () => {
         <link href="/images/w_logo.png" rel="icon" />
       </Head>
 
-      <div className="min-h-screen flex items-center justify-center py-8 px-4">
+      <div className="min-h-screen flex flex-col items-center justify-center px-4 gap-16">
+        <span className=" text-mikado-100 text-xl text-center leading-relaxed">
+          Migrate your packets from Ethereum to Skale
+          <br />
+          Connect Wallet and Open your Packets
+          <br />
+          <a
+            href={"https://www.wits.academy/"}
+            className="underline-offset-2 underline"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://www.wits.academy/
+          </a>
+        </span>
+
         <Claim />
       </div>
     </MainLayout>

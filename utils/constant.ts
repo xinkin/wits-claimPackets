aafea3d4a4d39b81057d9b8a6d12962595e8346e
encrypted8532 | mihir <mihirsinh.parmar.it@gmail.com>
2025-02-03 05:15:31 +0530

    Update constant.ts to use latest packets address

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,5 +1,5 @@
 export const deployedContractAddress =
-  "0x700cfC4379B795F3e6955e50A307709203EeEEC1";
+  "0xac6fA954a7F9879033B48ddB766024c1D16fF180";
 
 export const abstractLinkingPortal = "https://link.abs.xyz/";
 export const abstractTestnetRPC = "https://api.mainnet.abs.xyz";

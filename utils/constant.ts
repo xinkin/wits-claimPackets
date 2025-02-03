c4af36f324fb1cbe1d535c42a1ba28a5fccbce7c
Yash <91452674+xinkin@users.noreply.github.com>
2025-02-03 23:10:34 +0530

    updated paymaster

--- utils/constant.ts
+++ utils/constant.ts
@@ -2,4 +2,5 @@ export const deployedContractAddress =
   "0xac6fA954a7F9879033B48ddB766024c1D16fF180";
 
 export const abstractLinkingPortal = "https://link.abs.xyz/";
-export const PAYMASTER_ADDRESS = "0x94C09162F514A86b458Ba13beC0899ffb68A19c7";
+// export const PAYMASTER_ADDRESS = "0x94C09162F514A86b458Ba13beC0899ffb68A19c7";
+export const PAYMASTER_ADDRESS = "0x5f75695Ec3E41C48D415726B4C1A3eB00F2FFCEE";

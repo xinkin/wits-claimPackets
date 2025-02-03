4255ed2a1994cc5e3ea0b45a03311c358f2866e5
encrypted8532 | mihir <mihirsinh.parmar.it@gmail.com>
2025-02-03 05:47:23 +0530

    fix: update to use abstract mainnet

--- utils/checkIsClaimed.ts
+++ utils/checkIsClaimed.ts
@@ -1,11 +1,11 @@
 import { ethers } from "ethersv5";
 import { deployedContractAddress } from "../utils/constant";
 import ABI from "./abi.json";
-import { abstractTestnet } from "viem/chains";
+import { abstractTestnet, abstract } from "viem/chains";
 
 export async function checkIsPacketClaimed(leaf: string) {
   const provider = new ethers.providers.JsonRpcProvider(
-    abstractTestnet?.rpcUrls?.default.http[0],
+    abstract?.rpcUrls?.default.http[0],
   );
 
   const abi = ABI;
